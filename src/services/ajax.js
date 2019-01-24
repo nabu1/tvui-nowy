02d20618600bad1ad66a74c84237a81660d2f42a
nabu1 <nabu1312@gmail.com>
2019-01-24 02:06:41 +0100

    WiP cd

--- src/services/ajax.js
+++ src/services/ajax.js
@@ -26,9 +26,21 @@ export const ajaxGetSelectedPrograms = (context, { day, startHour, endHour, sele
 
   const arrSelectedCategories = JSON.stringify(selectedCategories)
 
-  const query1 = `q={$and:[{timestamp:{$gt:${startHour}}},{timestamp:{$lt:${endHour}},`
-  const query2 = selectedCategories && selectedCategories.length ? `category:{$in:${arrSelectedCategories}}}]}` : ''
-  const query3 = `}]}`
+  const query1 = `q={$and:[{timestamp:{$gt:${startHour}}},{timestamp:{$lt:${endHour}}`
+  const query2 = selectedCategories && selectedCategories.length ? `,category:{$in:${arrSelectedCategories}}}]}}` : ''
+  //const query3 = `}]}`
+  const query3 = ``
+
+
+  'https://api.mlab.com/api/1/databases/tvui/collections/tvui1?q={$and:[{timestamp:{$gt:1548298800000}},{timestamp:{$lt:1548316800000},category:{$in:[%22film%22,%22serial%22]}}]}}]}&apiKey=XRr-4BkluC11FFgtbOnUhzUlodvp8RfI'
+
+
+
+
+
+
+
+
 
   const url = constants.TV_LIST_PREFIX + query1 + query2 + query3 + constants.TV_LIST_SUFFIX
 
