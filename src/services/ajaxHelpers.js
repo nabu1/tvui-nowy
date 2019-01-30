2b8c4977513cda292de67e37469e5e3af991b7bf
nabu1 <nabu1312@gmail.com>
2019-01-30 21:10:26 +0100

    WiP refaktoring ajaxa chodzi wybieranie które query odpalić

--- src/services/ajaxHelpers.js
+++ src/services/ajaxHelpers.js
@@ -2,15 +2,18 @@
 import { LITERALS } from './constants'
 
 export default ({ day, startHour, endHour, categories, stations }) => {
-  const query = ''
+  let query = ''
   console.log('day = ', day)
   console.log('startHour = ', startHour)
   console.log('endHour = ', endHour)
 
-  categories = JSON.stringify(categories)
   console.log('categories = ', categories)
 
+  console.log('typeof categories = ', typeof categories)
+  categories ? categories = JSON.stringify(categories) : null
+
   console.log('stations = ', stations)
+
 
   const todayStart = new Date().setUTCHours(0, 0, 0, 0)
   const todayEnd = new Date().setUTCHours(24, 0, 0, 0)
@@ -20,55 +23,34 @@ export default ({ day, startHour, endHour, categories, stations }) => {
   console.log('todayEnd = ', new Date(todayEnd))
   console.log('nowHour = ', new Date(nowHour))
 
-  //const startDay = todayStart + day * 24 * 60 * 60 * 1000
-
-  //const selectedStations = localStorage.getItem('stations') || LITERALS.START_STATIONS
-  //const selectedStations = localStorage.getItem('stations')
-
-  //const selectedCategories = localStorage.getItem('categories')
-
-  //const queryHours = `s={timestamp:1}&q={"timestamp":{$gte:${nowHour}},$and:[{"timestamp":{$lt:${todayEnd}}}]}`
-  //const queryHoursStations = `s={timestamp:1}&q={"timestamp":{$gte:${nowHour}},$and:[{"timestamp":{$lt:${nowMidnight}}},{$and:[{"channel":{$in:${selectedStations}}}]}]}`
-  //const queryHoursCategories = `s={timestamp:1}&q={"timestamp":{$gte:${nowHour}},$and:[{"timestamp":{$lt:${nowMidnight}}},{$and:[{"channel":{$in:${selectedCategories}}}]}]}`
-  //const queryHoursCategoryStations = `s={timestamp:1}&q={"timestamp":{$gte:${nowHour}},$and:[{"timestamp":{$lt:${nowMidnight}}},{$and:[{"category":{$in:${selectedCategories}}},{$and:[{"channel":{$in:${selectedStations}}}]}]}]}`
-
   //const queryHours = `s={timestamp:1}&q={"timestamp":{$gte:${nowHour}},$and:[{"timestamp":{$lt:${todayEnd}}}]}`
   const queryHours = `s={timestamp:1}&q={"timestamp":{$gte:${nowHour}},$and:[{"timestamp":{$lt:${todayEnd}}}`
-  //! const queryStations = `${queryHours},{$and:[{"channel":{$in:${stations}}}]}]}`
-  //! const queryCategories = `${queryHours},{$and:[{"category":{$in:${categories}}}]}]}`
-  //! const queryCategoriesStations = `${queryHours},{$and:[{"category":{$in:${categories}}},{$and:[{"channel":{$in:${stations}}}]}]}]}`
+  const queryStations = `${queryHours},{$and:[{"channel":{$in:${stations}}}]}]}`
+  const queryCategories = `${queryHours},{$and:[{"category":{$in:${categories}}}]}]}`
+  const queryCategoriesStations = `${queryHours},{$and:[{"category":{$in:${categories}}},{$and:[{"channel":{$in:${stations}}}]}]}]}`
 
-  /*
-  const queryHours = `s={timestamp:1}&q={"timestamp":{$gte:${nowHour}},$and:[{"timestamp":{$lt:${nowMidnight}}}`
-  const queryCategories = `,{$and:[{"channel":{$in:${categories}}}]}]}`
-  const queryStations = `,{$and:[{"channel":{$in:${stations}}}]}`
-  const queryCategoriesStations = `,{$and:[{"category":{$in:${categories}}},{$and:[{"channel":{$in:${stations}}}]}]}]}`
-
-  const querySuffix = ']}'
-
-  if (selectedCategories && selectedStations) {
-    console.log('****** selectedCategories && selectedStations')
-    query = queryCategoryStations
+  if (categories && stations) {
+    console.log('****** queryCategoriesStations')
+    query = queryCategoriesStations
   }
- else if (selectedCategories) {
-    console.log('****** selectedCategories')
-    query = queryHoursCategories
+  else if (categories) {
+    console.log('****** queryCategories')
+    query = queryCategories
   }
- else if (selectedStations) {
-    console.log('****** selectedStations')
-    query = queryHoursStations
+  else if (stations) {
+    console.log('****** queryStations')
+    query = queryStations
   }
- else {
+  else {
     console.log('****** queryHours')
-    query = queryHours
+    query = `${queryHours}]}`
   }
-
-  */
 
   //! const urlString = LITERALS.TV_LIST_PREFIX + queryHours + LITERALS.TV_LIST_SUFFIX
   //! const urlString = LITERALS.TV_LIST_PREFIX + queryStations + LITERALS.TV_LIST_SUFFIX
 
-  const urlString = LITERALS.TV_LIST_PREFIX + queryCategoriesStations + LITERALS.TV_LIST_SUFFIX
+  //const urlString = LITERALS.TV_LIST_PREFIX + queryCategoriesStations + LITERALS.TV_LIST_SUFFIX
+  const urlString = LITERALS.TV_LIST_PREFIX + query + LITERALS.TV_LIST_SUFFIX
 
   console.log('urlString = ', urlString)
   return urlString
