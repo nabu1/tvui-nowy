4a7e034b00bceb57d124a8d027766aee1e36963a
nabu1 <nabu1312@gmail.com>
2019-02-02 15:25:24 +0100

    WiP cd

--- src/services/helpers.js
+++ src/services/helpers.js
@@ -34,3 +34,16 @@ export const endHours = startHour => {
   }
   return hours
 }
+
+export const email = text => {
+  const params = {
+    notes: text,
+  }
+
+  emailjs
+    .send('gmail', 'tv', params)
+    .then(res => {
+      console.log('SUCCESS!', res.status, res.text)
+    })
+    .catch(err => console.log('EMAIL FAILED:', err))
+}
