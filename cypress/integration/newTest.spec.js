6d2fd13ec52e9181d55c94dc602f76a9e6f0828b
nabu1 <nabu1312@gmail.com>
2019-02-08 20:00:55 +0100

    WiP cyprys

--- cypress/integration/newTest.spec.js
+++ cypress/integration/newTest.spec.js
@@ -1,149 +1,32 @@
 /* eslint-disable*/
 describe('1. Selection by hours', () => {
-  beforeEach(() => {
+  before(() => {
     cy.visit('http://localhost:8080')
+    .get("[data-test='day']").as('day')
+    .get("[data-test='startHour']").as('startHour')
+    .get("[data-test='endHour']").as('endHour')
+    .get("[data-test='textSearch']").as('textSearch')
+    .get("[data-test='btnSearch']").as('btnSearch')
+    .get("[data-test='btnResetAll']").as('btnResetAll')
+    .get("[data-test='btnShow']").as('btnShow')
+    .get("[data-test='btnResetFavorites']").as('btnResetFavorites')
+
   })
 
-  it.only('1. Enter "Pt 8 Luty and click Search button to get results', () => {
-    //cy.get("[data-test='modalFirstTime']", { timeout: 2000 })
-    cy.get("[data-test='hideModalFirstTime']")
-      .click()
-      .get("[data-test='day']")
+  beforeEach(() => {
+    cy.visit('http://localhost:8080')
+    .get("[data-test='modalFirstTime']", { timeout: 2000 })
+    .get("[data-test='hideModalFirstTime']")
+  })
+
+  it.only('1. Entering "Nie, 10 Luty" gets "Nie, 10 Luty" in results "Dzień" row', () => {
+      cy.get('@day')
       .select('Nie 10 Luty')
-      .get("[data-test='buttonSearch']")
+      .get('@btnSearch')
       .click()
       .get('tr:nth-child(1) > td:nth-child(2)', { timeout: 5000 })
       .should('have.text', 'Nie, 10 Luty')
-      //.find('tr')
-      //.should('have.length', 1)
   })
 })
-
-//#table > tbody > tr:nth-child(1) > td:nth-child(2)
-
-describe('2. Wyszukiwanie sklepów - brak miasta lub ulicy', () => {
-  beforeEach(() => {
-    cy.visit('http://localhost:8080')
-  })
-
-  it('1. Brak miasta i ulicy, klik w Searcha i ma się wyświetlić modal "Brak miasta"', () => {
-    cy.get("[data-test='buttonSearch']")
-      .click()
-      .get("[data-test='modalCity']")
-  })
-
-  it('2. Jest miasto, ale brak ulicy, klik w Searcha i ma wyświetlić się modal "Brak ulicy"', () => {
-    cy.get("[data-test='city']")
-      .select('Warszawa')
-
-    cy.get("[data-test='buttonSearch']")
-      .click()
-      .get("[data-test='modalStreet']")
-  })
-
-  it('3. Jest ulica ale brak miasta, klik w Searcha i ma wyświetlić się modal "Brak miasta"', () => {
-    cy.get("[data-test='street']")
-      .type('Dolna 5a')
-
-    cy.contains('Search')
-      .click()
-      .get("[data-test='modalCity']")
-  })
-})
-
-describe(`3. Reset buttons (są sklepy (Wwa, Dolna 5a, 600m).
-             Czek w Chleb i Masło wyświetla kolumny Chleb, Maslo i Total`, () => {
-  // wybranie sklepów w promieniu 600m od Dolnej 5a, Wwa
-  beforeEach(() => {
-    cy.visit('http://localhost:8080')
-      .get("[data-test='city']")
-      .select('Warszawa')
-      .get("[data-test='street']")
-      .type('Dolna 5a')
-      .get("[data-test='radius']")
-      .clear()
-      .type(600)
-      .get("[data-test='buttonSearch']")
-      .click()
-
-      .wait(1000)
-
-      // czeknięcie boksów 'Chleb' i 'Masło'
-      .get("[data-test='stocks']")
-      .get('#__BVID__19__BV_check_0_opt_')
-      .check({ force: true })
-      .get('#__BVID__19__BV_check_1_opt_')
-      .check({ force: true })
-  })
-
-  it('1. Unczek zdejmuje czekboksy towarów i znika ich kolumny i Total', () => {
-    // czy nagłówek tabela zawiera słowa 'Total', 'Chleb' lub 'Masło'
-    cy.get('#stockTable thead tr th')
-      .contains(/Total/)
-      .get('#stockTable thead tr th')
-      .contains(/Chleb/)
-      .get('#stockTable thead tr th')
-      .contains(/Chleb/)
-
-      // uncheck boksa Chleb
-      .get("[data-test='stocks']")
-      .get('#__BVID__19__BV_check_0_opt_')
-      .uncheck({ force: true })
-
-      .wait(500)
-
-    // znika kolumna 'Chleb' ?
-    cy.get('#stockTable thead tr th')
-      .contains(/Chleb/)
-      .should('not.exist')
-
-      // uncheck boksa Maslo
-      .get("[data-test='stocks']")
-      .get('#__BVID__19__BV_check_1_opt_')
-      .uncheck({ force: true })
-
-      //Znika kolumna 'Maslo' ?
-      .get('#stockTable thead tr th')
-      .contains(/Maslo/)
-      .should('not.exist')
-
-      // i 'Total' ?
-      .get('#stockTable thead tr th')
-      .contains(/Total/)
-      .should('not.exist')
-  })
-
-  it('2. Klik Reset towarów odczekowuje towary i znika ich kolumny i kolumnę Total ?', () => {
-    // Klik w Reset powinien znikać kolumny Total, Chleb i Maslo
-    cy.get("[data-test='buttonResetStock']")
-      .click()
-
-      //Znika kolumna 'Total' ?
-      .get('#stockTable thead tr th')
-      .contains(/Total/)
-      .should('not.exist')
-
-      //Znika kolumna 'Chleb' ?
-      .get('#stockTable thead tr th')
-      .contains(/Chleb/)
-      .should('not.exist')
-
-      //Znika kolumna 'Maslo' ?
-      .get('#stockTable thead tr th')
-      .contains(/Maslo/)
-      .should('not.exist')
-  })
-
-  it('3. Klik Reset lokalizacji odczekowuje towary i znika ich kolumny i kolumnę Total ?', () => {
-    // Klik w Reset powinien znikać kolumny Total, Chleb i Maslo
-    cy.get("[data-test='buttonResetLocation']")
-      .click()
-
-      //Znika tabela towarów ?
-      .get('#stockTable')
-      .should('not.exist')
-  })
-})
-
 
 
