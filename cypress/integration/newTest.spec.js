487bb6e73550c452e32453dca7dde31d4bf28c9d
nabu1 <nabu1312@gmail.com>
2019-02-09 00:28:37 +0100

    WiP cyprys

--- cypress/integration/newTest.spec.js
+++ cypress/integration/newTest.spec.js
@@ -1,6 +1,5 @@
 /* eslint-disable*/
-describe('1. Selection by hours', () => {
-  before(() => { })
+describe('I. Selection by hours', () => {
   beforeEach(() => {
     cy.visit('http://localhost:8080')
     .get("[data-test='day']").as('day')
@@ -30,16 +29,82 @@ describe('1. Selection by hours', () => {
       .should('have.text', 'Nie, 10 Luty')
   })
 
-  it.only('3. Entering 3rd option gets "Nie, 10 Luty" in results "Dzień" row', () => {
+  it('3. Entering 23 in startHour gets results starting with "22" in "Czas" column', () => {
     cy.get('@startHour')
-    .select('22')
+    .select('23')
     .get('@btnSearch')
     .click()
     .get('#table > tbody > tr:nth-child(1) > td:nth-child(4)')
-    .should('have.text', '22:00')
-    //.should('match', /22:00/)
+    .contains(/23/)
+    .should('exist')
+  })
+
+  it('4. Entering "Sob 9 Luty" and "6" as endHour gets results \
+          ending with "4" or "5" in "Czas" column', () => {
+
+    cy.get('@day')
+    .select('Sob 9 Luty')
+    .get('@endHour')
+    .select('6')
+    .get('@btnSearch')
+    .click()
+    .get('#table > tbody > tr:last > td:nth-child(4)')
+    .contains(/[4,5].*/)
+    .should('exist')
+    //.should('have.text', '5:59')
+  })
+
+  it('5. Entering "Pon 11 Luty" and "10" as startHour and "14" as endHour \
+              gets results starting with "10" and "12" or "13" in "Czas" column', () => {
+
+    cy.get('@day')
+    .select('Pon 11 Luty')
+    .get('@startHour')
+    .select('10')
+    .get('@endHour')
+    .select('14')
+    .get('@btnSearch')
+    .click()
+    .get('#table > tbody > tr:first > td:nth-child(4)')
+    .contains(/10.*/)
+    .should('exist')
+    .get('#table > tbody > tr:last > td:nth-child(4)')
+    .contains(/[12,13].*/)
+    .should('exist')
+    .get('@btnResetAll')
+    .click()
   })
 })
 
-// #table > tbody > tr:nth-child(1) > td:nth-child(4)
-// #table > tbody > tr:nth-child(1) > td:nth-child(4)
+describe('II. Selection by categories', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:8080')
+    .get("[data-test='day']").as('day')
+    .get("[data-test='startHour']").as('startHour')
+    .get("[data-test='endHour']").as('endHour')
+    .get("[data-test='textSearch']").as('textSearch')
+    .get("[data-test='btnSearch']").as('btnSearch')
+    .get("[data-test='btnResetAll']").as('btnResetAll')
+    .get("[data-test='btnShow']").as('btnShow')
+    .get("[data-test='btnResetFavorites']").as('btnResetFavorites')
+    .get("[data-test='table']").as('table')
+  })
+
+  it.only('1. Checking "Inne" gets "inne" as a category name in the 1st \
+         and last row of the table', () => {
+    cy.get('@day')
+    .select('Pon 11 Luty')
+    .get('@btnSearch')
+    .click()
+    .get('#categories > div:nth-child(7) > label > span')
+    .click()
+    .get('#table > tbody > tr:first > td:nth-child(7)')
+    .contains('inne')
+    .should('exist')
+    .get('#table > tbody > tr:last > td:nth-child(7)')
+    .contains('inne')
+    .should('exist')
+  })
+})
+
+// #categories > div:nth-child(7) > label > span
