de5448a7702823f4227659b49e7ffd41b5ba6c6f
nabu1 <nabu1312@gmail.com>
2019-02-10 18:29:41 +0100

    WiP cypress

--- cypress/integration/newTest.spec.js
+++ cypress/integration/newTest.spec.js
@@ -12,76 +12,47 @@ describe('I. Selection by hours', () => {
     .get("[data-test='btnResetFavorites']").as('btnResetFavorites')
   })
 
-  it('1. Entering nothing gets "Sob, 9 Luty" in results "Dzień" row', () => {
+  it('1. Entering nothing gets "Nie, 10 Luty" in results "Dzień" row', () => {
     cy.get('@btnSearch')
       .click()
       .get('tr:nth-child(1) > td:nth-child(2)', { timeout: 8000 })
       .should('have.text', 'Nie, 10 Luty')
   })
 
-  it.only('2. Entering "Nie, 10 Luty" gets "Nie, 10 Luty" in results "Dzień" row', () => {
-     const click = $el => $el.click()
+  it('2. Entering "Nie, 10 Luty" gets "Nie, 10 Luty" in results "Dzień" row', () => {
+      cy.get('@day').select('Pon 11 Luty')
+      .get('@btnSearch').click()
 
-      cy.get('@day')
-      .wait(1500)
-      .select('Pon 11 Luty')
-      .wait(1500)
-      .get('@btnSearch')
-      .wait(1500)
-      //.click()
-      .pipe(click)
+      .get('spinner').should('not.be.visible')
 
-      //.get('spinner').should('not.be.visible')
-
-      .get('tr:nth-child(1) > td:nth-child(2)', { timeout: 5000 })
-      .should('be.visible')
-      .should('have.text', 'Pon, 11 Luty')
+      .get('tr:nth-child(1) > td:nth-child(2)', { timeout: 6000 }).should('be.visible').should('have.text', 'Pon, 11 Luty')
+      .should('be.visible').get('tr:nth-child(1) > td:nth-child(2)').should('have.text', 'Pon, 11 Luty')
   })
+
 
   it('3. Entering 23 in startHour gets results starting with "22" in "Czas" column', () => {
-    cy.get('@startHour')
-    .select('23')
-    .get('@btnSearch')
-    .click()
-    .get('#table > tbody > tr:nth-child(1) > td:nth-child(4)')
-    .contains(/23/)
-    .should('exist')
+    cy.get('@startHour').select('23').wait(2000)
+    .get('@btnSearch').click()
+    .get('#table > tbody > tr:nth-child(1) > td:nth-child(4)', { timeout: 6000 }).contains(/23/).should('exist')
   })
 
-  it('4. Entering "Sob 9 Luty" and "6" as endHour gets results \
+  it('4. Entering "Sob Wt 12 Luty" and "6" as endHour gets results \
           ending with "4" or "5" in "Czas" column', () => {
-
-    cy.get('@day')
-    .select('Pon 11 Luty')
-    .get('@endHour')
-    .select('6')
-    .get('@btnSearch')
-    .click()
-    .get('#table > tbody > tr:last > td:nth-child(4)')
-    .contains(/[4,5].*/)
-    .should('exist')
-    //.should('have.text', '5:59')
+    cy.get('@day').select('Wt 12 Luty').wait(1000)
+    .get('@endHour').select('6').wait(1000)
+    .get('@btnSearch').click()
+    .get('#table > tbody > tr:last > td:nth-child(4)', { timeout: 6000 }).contains(/5.*/).should('exist')
   })
 
   it('5. Entering "Pon 11 Luty" and "10" as startHour and "14" as endHour \
               gets results starting with "10" and "12" or "13" in "Czas" column', () => {
 
-    cy.get('@day')
-    .select('Pon 11 Luty')
-    .get('@startHour')
-    .select('10')
-    .get('@endHour')
-    .select('14')
-    .get('@btnSearch')
-    .click()
-    .get('#table > tbody > tr:first > td:nth-child(4)')
-    .contains(/10.*/)
-    .should('exist')
-    .get('#table > tbody > tr:last > td:nth-child(4)')
-    .contains(/[12,13].*/)
-    .should('exist')
-    .get('@btnResetAll')
-    .click()
+    cy.get('@day').select('Pon 11 Luty')
+    .get('@startHour').select('10')
+    .get('@endHour').select('14').wait(2000)
+    .get('@btnSearch').click()
+    .get('#table > tbody > tr:first > td:nth-child(4)', { timeout: 6000 }).contains(/9.*/).should('exist')
+    .get('#table > tbody > tr:last > td:nth-child(4)').contains(/[12,13].*/).should('exist')
   })
 })
 
@@ -145,30 +116,17 @@ describe('III. Selection by hours and categories', () => {
     .get("[data-test='btnResetFavorites']").as('btnResetFavorites')
   })
 
-  it('1. Entering "Pon, 11 Luty", checking "Serial" and "Inne" gets "serial" or "inne" \
+  it.only('1. Entering "Pon, 11 Luty", checking "Serial" and "Inne" gets "serial" or "inne" \
               as a category name in the 1st and last row of the table', () => {
 
-      cy.get('@day')
-      .pause()
+      cy.get('@day').select('Wt 12 Luty').wait(2000)
+      .get('@btnSearch').click()
+      //.get('spinner').should('not.be.visible')
+      .get('#categories > div:nth-child(1) > label > span').click()
+      .get('#categories > div:nth-child(2) > label > span').click()
 
-      .select('Wt 12 Luty')
-      .get('@btnSearch')
-      .click()
-
-      .get('spinner').should('not.be.visible')
-
-      .get('#categories > div:nth-child(1) > label > span')  // Film
-      .click()
-      .get('#categories > div:nth-child(2) > label > span')  // Serial
-      .click()
-
-      .get('#table > tbody > tr:first > td:nth-child(7)')
-      .contains('serial' || 'film')
-      .should('exist')
-
-      .get('#table > tbody > tr:last > td:nth-child(7)')
-      .contains('film' ||'serial')
-      .should('exist')
+      .get('#table > tbody > tr:first > td:nth-child(7)', { timeout: 6000 }).contains('serial').should('exist')
+      .get('#table > tbody > tr:last > td:nth-child(7)').contains('film').should('exist')
   })
 
 })
