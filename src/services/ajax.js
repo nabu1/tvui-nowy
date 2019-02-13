8106c9262ab905df59c1340b1b14cb2fd7eac90c
nabu1 <nabu1312@gmail.com>
2019-02-13 22:35:00 +0100

    Done - textSearch with hours

--- src/services/ajax.js
+++ src/services/ajax.js
@@ -27,51 +27,24 @@ export const ajaxFindText = (context, text) => {
   let startHour = context.getters.getStartHour
   const endHour = context.getters.getEndHour || 24
 
-
   if (!startHour && (!day || new Date(day).getDate() === new Date().getDate())) {
     startHour = new Date().getHours()
   }
 
   const start = startOfDay + startHour * 60 * minutes - 30 * minutes
-
-  console.log('%c startHour = ' + startHour, 'color: orange')
-  console.log('%c start = ' + new Date(start), 'color: orange')
-  console.log('%c new Date().getMinutes() - 30 = ' + (new Date().getMinutes() - 30), 'color: white')
-
   const end = startOfDay + endHour * 60 * minutes
-  console.log('%c end = ' + new Date(end), 'color: orange')
-
-  ///query = `q={$or:[{"type":{"$regex":".*${text}.*",$options:"i"}},{"title":{"$regex":".*${text}.*",$options:"i"}}]}`
-  /// const queryText = `s={timestamp:1}&q={$or:[{"type":{"$regex":".*${text}.*",$options:"i"}},{"title":{"$regex":".*${text}.*",$options:"i"}}]}`
-  /// const queryTextFragment = `{$or:[{"type":{"$regex":".*${text}.*",$options:"i"}},{"title":{"$regex":".*${text}.*",$options:"i"}}]}`
-  /// const queryText = `q={$or:[{"type":{"$regex":".*${text}.*",$options:"i"}},{"title":{"$regex":".*${text}.*",$options:"i"}}]}`
 
   const queryTextFragment = `$or:[{"type":{"$regex":".*${text}.*",$options:"i"}},{"title":{"$regex":".*${text}.*",$options:"i"}}]`
   const queryHoursText = `s={timestamp:1}&q={"timestamp":{$gte:${start}},$and:[{"timestamp":{$lte:${end}}}],` + queryTextFragment + `}`
   const queryText = `s={timestamp:1}&q={$or:[{"type":{"$regex":".*${text}.*",$options:"i"}},{"title":{"$regex":".*${text}.*",$options:"i"}}]}`
 
-  console.log('%c day = ' + day, 'color: orange')
-  console.log('%c day = ' + new Date(day), 'color: orange')
-  console.log('%c context.getters.getStartHour = ' + context.getters.getStartHour, 'color: orange')
-  console.log('%c  context.getters.getEndHour = ' +  context.getters.getEndHour, 'color: orange')
-
   query = !day && !context.getters.getStartHour && !context.getters.getStartHour ? queryText : queryHoursText
 
-
-  console.log('%c queryHoursText = ' + queryHoursText, 'color: lime')
-
-
-  /// const queryHours = `s={timestamp:1}&q={"timestamp":{$gte:${start}},$and:[{"timestamp":{$lte:${end}}}`
-
-
-  /// const urlFindText = LITERALS.TV_LIST_PREFIX + queryHoursText + LITERALS.TV_LIST_SUFFIX
-  //const urlFindText = LITERALS.TV_LIST_PREFIX + queryText + LITERALS.TV_LIST_SUFFIX
-
-  const urlFindText = LITERALS.TV_LIST_PREFIX + query + LITERALS.TV_LIST_SUFFIX
-  console.log('%c urlFindText = ' + urlFindText, 'color: white')
+  const urlTextSearch = LITERALS.TV_LIST_PREFIX + query + LITERALS.TV_LIST_SUFFIX
+  console.log('%c urlTextSearch = ' + urlTextSearch, 'color: lime')
 
   axios
-    .get(urlFindText)
+    .get(urlTextSearch)
     .then(res => {
       console.log('%c res.data = ' + res.data.length, 'color: white')
       context.commit('GET_DOCUMENTS_COUNT', res.data.length)
